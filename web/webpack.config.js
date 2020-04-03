6e7a75b99518d660f71219d230f193c11fbd450b
lutzer <in_the_fishtank@gmx.de>
2020-04-03 21:31:18 +0200

    setup web interface build environment

---
+++ web/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, argv) => {
+
+  const builDir = './dist/'
+  console.log(path.resolve(__dirname, builDir))
+
+  return {
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    entry: './src/index.tsx',
+  
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, builDir),
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Planetary Sequencer',
+        template: 'src/index.html'
+      })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+  
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    devServer: {
+      port: 3001,
+      open: true,
+      stats: {
+        children: false, // Hide children information
+        maxModules: 0
+      }
+    },
+  }
+} 
