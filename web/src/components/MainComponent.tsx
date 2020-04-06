e396c9c50cef0a0f7c46f15c725bd228fb701efd
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2020-04-06 16:09:39 +0200

    mapFromMapbox

--- web/src/components/MainComponent.tsx
+++ web/src/components/MainComponent.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Hello } from "./Hello";
 import { MapComponent } from "./MapComponent";
 
 const MainComponent = () => {
   return (
-    <div>
-      <Hello compiler="Typescript" framework="React"/>
+    
       <MapComponent/>
-    </div>
+    
   )
 }
 
--- web/src/components/MapComponent.tsx
+++ web/src/components/MapComponent.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
-
+import {render} from 'react-dom';
 import './styles/map.scss'
+const MAPBOX_TOKEN="pk.eyJ1Ijoibmlub2dsb250aSIsImEiOiJjazhvYzR1aWUwMWZqM2xtejA2b2Jvd2FrIn0.oIyykX1DyYDJ6jZKNyg0Nw"
 
 class MapComponent extends Component {
 
   state = {
+
     viewport: {
-      width: 400,
-      height: 400,
+      width: "100vw",
+      height: "100vh",
       latitude: 37.7577,
       longitude: -122.4376,
-      zoom: 8
+      zoom: 2
     }
   };
 
   render() {
     return (
       <div>
-        <h1 className="map text">Map</h1>
         <ReactMapGL
         {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}/>
+        onViewportChange={(viewport) => this.setState({viewport})} 
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+
+
+
+        />
       </div>
       
     );
