fd70782b4acfcb3d7611215a41cdd9f7d490d439
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2020-04-20 12:03:15 +0200

    stories

--- web/src/components/ProjectsComponent.jsx
+++ web/src/components/ProjectsComponent.jsx
@@ -1,51 +1,64 @@
 import React from "react";
-import { Component
-} from 'react';
-import ReactMapGL from 'react-map-gl';
-import axios from "axios"
+import { Component } from "react";
+import ReactMapGL from "react-map-gl";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const projectDefaults = {
-    id : "",
-    name : "" ,
-    description : '',
-    visible : true,
-    createdAt : 0
-}
+  id: "",
+  name: "",
+  description: "",
+  visible: true,
+  createdAt: 0,
+};
 
 class ProjectsComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            project : projectDefaults,
-            stories : []
-        }
-    }
-    
-    //GET THE PROJECT FROM API
-    getProject () {
-        const { projectName } = this.props.match.params
-        axios.get(`http://localhost:3000/api/projects/?name=${projectName}`)
-        .then(responseFromApi => {
-            this.setState({ project: Object.assign({}, projectDefaults, responseFromApi.data.project ) })
-            return axios.get(`http://localhost:3000/api/stories/?project=${this.state.project.id}`)
-        }).then( (response) => {
-           console.log(response.data) 
-        })  
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: projectDefaults,
+      stories: [],
+    };
+  }
 
-    componentDidMount() {
-        this.getProject()
-    }
-    
-    render() {
+  //GET THE PROJECT FROM API
+  getProject() {
+    const { projectName } = this.props.match.params;
+    axios
+      .get(`http://localhost:3000/api/projects/?name=${projectName}`)
+      .then((responseFromApi) => {
+        this.setState({
+          project: Object.assign(
+            {},
+            projectDefaults,
+            responseFromApi.data.project
+          ),
+        });
+        return axios.get(
+          `http://localhost:3000/api/stories/?project=${this.state.project.id}`
+        );
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }
 
-        return (
-            <div key= {this.state._id}>
-              <h3>{this.state.project.name}</h3>
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.getProject();
+  }
+  render() {
+    return (
+      <div>
+        <div key={this.state._id}>
+          <h3>{this.state.project.name}</h3>
+          <h2>{this.state.project.id}</h2>
+        </div>
+        <div>
+      {this.state.stories.map((story) => <h1>{story.author}</h1>)}
+        </div>
+      </div>
+    );
+  }
 }
 
-export {ProjectsComponent}
+export { ProjectsComponent };
