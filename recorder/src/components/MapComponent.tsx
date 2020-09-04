f632ffcc5694929c73cc47cc4420b5dc3eee958d
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2020-09-04 17:02:31 +0200

    map component initial changes

--- recorder/src/components/MapComponent.tsx
+++ recorder/src/components/MapComponent.tsx
@@ -1,19 +1,55 @@
 import React, { useState } from "react";
 import { ApiStorySchema } from "../services/api";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { config } from "../config";
+import "./styles/map.scss";
 
 type MapProps = {
-  stories : ApiStorySchema[],
-  onMarkerClick : (storyId : string) => void
-}
+  stories: ApiStorySchema[];
+  onMarkerClick: (storyId: string) => void;
+};
 
-const MapComponent = ({ stories, onMarkerClick } : MapProps) => {
-  
-  return(
+const MapComponent = ({ stories, onMarkerClick }: MapProps) => {
+  const [viewport, setViewport] = useState({
+    latitude: 52.51763153076172,
+    longitude: 13.40965747833252,
+    zoom: 3,
+  });
+
+  const [story, setStory] = useState({
+    stories: { stories },
+  });
+
+  return (
     <div>
-      <div><button onClick={() => onMarkerClick('dlsfjl')}>Marker</button></div>
-      <div>Map: {JSON.stringify(stories)}</div>
+      <ReactMapGL
+        width="100vw"
+        height="100vh"
+        {...viewport}
+        mapboxApiAccessToken={config.mapboxToken}
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+        mapStyle="mapbox://styles/ninoglonti/ck99qscv60l0g1imgpdteyqfw"
+      >
+        {stories.map((story) => (
+          <Marker
+            key={story.id}
+            latitude={story.location[0]}
+            longitude={story.location[1]}
+          >
+            <button
+              className="marker-btn"
+              onClick={(story) => {
+                console.log("hey I am klicked", story);
+                //setStory({story.id});
+              }}
+            ></button>
+          </Marker>
+        ))}
+      </ReactMapGL>
     </div>
-  )
-}
+  );
+};
 
-export { MapComponent }
+export { MapComponent };
--- recorder/src/config.ts
+++ recorder/src/config.ts
@@ -1,13 +1,16 @@
-const config : {
-  apiAdress: string,
-  maxStories : number,
-  maxRecordingDuration: number,
-  defaultLocation : [number, number]
+const config: {
+  apiAdress: string;
+  maxStories: number;
+  maxRecordingDuration: number;
+  defaultLocation: [number, number];
+  mapboxToken: string;
 } = {
-  apiAdress : '/api/',
-  maxStories : 5,
+  apiAdress: "/api/",
+  maxStories: 5,
   maxRecordingDuration: 300, // in seconds
-  defaultLocation: [52.508239, 13.329132]
-}
+  defaultLocation: [52.508239, 13.329132],
+  mapboxToken:
+    "pk.eyJ1Ijoibmlub2dsb250aSIsImEiOiJjazhvYzR1aWUwMWZqM2xtejA2b2Jvd2FrIn0.oIyykX1DyYDJ6jZKNyg0Nw",
+};
 
-export { config }
+export { config };
